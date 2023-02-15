70157852fa84d740f81a8c411a5b5f967e97faa2
Isah Jacob <isahjacob0@gmail.com>
2023-02-15 20:44:04 +0100

    added model to the import statement

--- management/commands/generate_collect_table.py
+++ management/commands/generate_collect_table.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-from esusu_app import Contribution, Member
+from esusu_app.models import Contribution, Member
 
 
 class Command(BaseCommand):
